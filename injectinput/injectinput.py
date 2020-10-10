59e727fde911a8e7779d607789c4cc82e93765d9
Dick Marinus <dick@mrns.nl>
2020-10-10 21:14:12 +0200

    initialize with capabilities

--- injectinput/injectinput.py
+++ injectinput/injectinput.py
@@ -2,26 +2,29 @@ import evdev
 import sys
 import time
 
+
 def main():
-    with evdev.UInput() as ui:
+    with evdev.UInput(
+        evdev.util.find_ecodes_by_regex(r"KEY_([A-Z]|SPACE|LEFTSHIFT|ENTER)$")
+    ) as ui:
         escape = False
         for letter in sys.argv[1]:
-            if letter == ' ':
+            if letter == " ":
                 key = evdev.ecodes.KEY_SPACE
-            elif letter == '\\':
+            elif letter == "\\":
                 escape = True
                 continue
-            elif escape and letter == 'r':
+            elif escape and letter == "r":
                 escape = False
                 key = evdev.ecodes.KEY_ENTER
             else:
-                key = evdev.ecodes.ecodes['KEY_'+letter.upper()]
+                key = evdev.ecodes.ecodes["KEY_" + letter.upper()]
             if letter.isupper():
                 ui.write(evdev.ecodes.EV_KEY, evdev.ecodes.KEY_LEFTSHIFT, 1)
 
             ui.write(evdev.ecodes.EV_KEY, key, 1)
             ui.write(evdev.ecodes.EV_KEY, key, 0)
-            time.sleep(.05)
+            time.sleep(0.05)
             ui.syn()
 
             if letter.isupper():
