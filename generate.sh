1672d5c681afe7a5357b6d7982f46a6c23352c6f
Dick Marinus <dick@mrns.nl>
2023-11-09 20:46:37 +0100

    update python

--- generate.sh
+++ generate.sh
@@ -1,3 +1,3 @@
-sudo /usr/local/venv/injectinput-3.11/bin/pip install /home/$USER/git/injectinput/
+sudo /usr/local/venv/injectinput-3.12/bin/pip install /home/$USER/git/injectinput/
 cd /usr/local/venv
-sudo tar cf /home/$USER/injectinput-3.11.tar injectinput-3.11
+sudo tar cf /home/$USER/injectinput-3.12.tar injectinput-3.12
